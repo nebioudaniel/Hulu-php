8a0676c0b917fd47290655711395ce9c4f0e3901
Nebiou Daniel Berhane <124292845+nebioudaniel@users.noreply.github.com>
2024-03-29 01:37:34 +0300

    Update index.php

--- public/src/routes/index.php
+++ public/src/routes/index.php
@@ -1,16 +1,60 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/config.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
-$app = \Slim\Factory\AppFactory::create();
+$app->get('/api/products', function (Request $request, Response $response) {
+    global $productCollection;
+    
+    $products = $productCollection->find();
+    $productsArray = [];
 
-// MongoDB connection
-$mongoClient = new MongoDB\Client(CONFIG['mongo_uri']);
-$db = $mongoClient->marketplace;
-$productCollection = $db->products;
+    foreach ($products as $product) {
+        $productsArray[] = $product;
+    }
 
-// Define routes
-require_once __DIR__ . '/src/routes/index.php';
+    $response->getBody()->write(json_encode($productsArray));
+    return $response->withHeader('Content-Type', 'application/json');
+});
 
-$app->run();
+$app->get('/api/products/{id}', function (Request $request, Response $response, $args) {
+    global $productCollection;
+
+    $product = $productCollection->findOne(['_id' => new MongoDB\BSON\ObjectID($args['id'])]);
+
+    $response->getBody()->write(json_encode($product));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->post('/api/products', function (Request $request, Response $response) {
+    global $productCollection;
+
+    $data = $request->getParsedBody();
+    $insertResult = $productCollection->insertOne($data);
+
+    $response->getBody()->write(json_encode($insertResult->getInsertedId()));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->put('/api/products/{id}', function (Request $request, Response $response, $args) {
+    global $productCollection;
+
+    $data = $request->getParsedBody();
+    $updateResult = $productCollection->updateOne(
+        ['_id' => new MongoDB\BSON\ObjectID($args['id'])],
+        ['$set' => $data]
+    );
+
+    $response->getBody()->write(json_encode($updateResult->getModifiedCount()));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->delete('/api/products/{id}', function (Request $request, Response $response, $args) {
+    global $productCollection;
+
+    $deleteResult = $productCollection->deleteOne(['_id' => new MongoDB\BSON\ObjectID($args['id'])]);
+
+    $response->getBody()->write(json_encode($deleteResult->getDeletedCount()));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
