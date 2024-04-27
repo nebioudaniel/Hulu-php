74224ee814654678ba60d50c7bd02b029dc527bf
Nebiou Daniel Berhane <124292845+nebioudaniel@users.noreply.github.com>
2024-04-27 11:46:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
+// Here's how you could integrate the provided code snippet into your project, along with a commit message and description:
+
+
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 
-$app = \Slim\Factory\AppFactory::create();
+use Slim\Factory\AppFactory;
+use MongoDB\Client;
+
+$app = AppFactory::create();
 
 // MongoDB connection
-$mongoClient = new MongoDB\Client(CONFIG['mongo_uri']);
+$mongoClient = new Client(CONFIG['mongo_uri']);
 $db = $mongoClient->marketplace;
 $productCollection = $db->products;
 
@@ -14,3 +20,5 @@ $productCollection = $db->products;
 require_once __DIR__ . '/src/routes/index.php';
 
 $app->run();
+
+
